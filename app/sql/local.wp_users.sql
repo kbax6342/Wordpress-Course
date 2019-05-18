37d360815bfe65a7e455798d2e43e2ff0000ea3c
PW is password <pwispassword@BaxterDevPro.local>
2019-05-18 12:27:11 -0400

    practice git commands

---
+++ app/sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"kbax6342","$P$BLLuz8122pTNRB9Blix1odNjnIOOls0","kbax6342","k3vin.baxt3r@gmail.com","","2019-05-17 02:07:44","",0,"kbax6342");
