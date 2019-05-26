3e67b861b6794ce701d59f5d73021b2ec3715fb0
Kevin Baxter <pwispassword@BaxterDevPro.local>
2019-05-26 09:57:05 -0400

    ...

--- app/sql/local.wp_terms.sql
+++ app/sql/local.wp_terms.sql
@@ -2,4 +2,7 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_terms` VALUES
-(1,"Uncategorized","uncategorized",0);
+(1,"Uncategorized","uncategorized",0),
+(3,"My Main Header Menu","my-main-header-menu",0),
+(4,"My Magical Menu","my-magical-menu",0),
+(5,"Super Amazing Menu","super-amazing-menu",0);
